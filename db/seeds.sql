89f5bfaa85f860b65182427edeb620c607c55b64
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2020-09-18 17:48:14 -0400

    set up server and database

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Big Mic');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Hamburger', true);
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Double Cheeseburger');
