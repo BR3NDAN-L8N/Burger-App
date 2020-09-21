346e6afe7ffd54bfbcfb65eb84e99142d58c3247
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2020-09-20 23:56:13 -0400

    added database info for Jaws to be used with Heroku

--- config/connection.js
+++ config/connection.js
@@ -2,16 +2,20 @@
 const mysql = require("mysql");
 require('dotenv').config()
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: 3306,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: "burger_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: "burger_db"
+  });
+}
 
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
